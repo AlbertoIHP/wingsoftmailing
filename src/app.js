073291760f165239d620f5d84133ed50abe90d80
Alberto Ignacio <a.herrera07@ufromail.cl>
2018-04-06 14:21:53 -0300

    deploy segundo

--- src/app.js
+++ src/app.js
@@ -11,55 +11,55 @@ const app = express(apiRoot, api)
 const server = http.createServer(app)
 
 /**
-** SOCKET 
+** SOCKET
 **/
 
-import socketio from 'socket.io'
-const websocket = socketio(server)
+// import socketio from 'socket.io'
+// const websocket = socketio(server)
 
 
 
-const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.S3_REGION,
-});
+// const s3 = new aws.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.S3_REGION,
+// });
 
-// Initialize multers3 with our s3 config and other options
-const upload = multer({
-  storage: multerS3({
-    s3,
-    bucket: process.env.S3_BUCKET_NAME,
-    acl: 'public-read',
-    metadata(req, file, cb) 
-    {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key(req, file, cb) 
-    {
-      var fileType = file.originalname.split('.')[file.originalname.split('.').length - 1 ]
-      cb(null, Date.now().toString() + '.' + fileType);
-    }
-  })
-})
+// // Initialize multers3 with our s3 config and other options
+// const upload = multer({
+//   storage: multerS3({
+//     s3,
+//     bucket: process.env.S3_BUCKET_NAME,
+//     acl: 'public-read',
+//     metadata(req, file, cb)
+//     {
+//       cb(null, {fieldName: file.fieldname});
+//     },
+//     key(req, file, cb)
+//     {
+//       var fileType = file.originalname.split('.')[file.originalname.split('.').length - 1 ]
+//       cb(null, Date.now().toString() + '.' + fileType);
+//     }
+//   })
+// })
 
-app.post('/upload', upload.single('photo'), (req, res, next) => {
-  res.json(req.file)
-})
+// app.post('/upload', upload.single('photo'), (req, res, next) => {
+//   res.json(req.file)
+// })
 
-websocket.on( 'connection', (socket) => {
+// websocket.on( 'connection', (socket) => {
 
-// Aqui se definen todos los eventos que cualquier cliente escucha, broadcast emite el evento a TODOS los clientes, mientras
-// que emit, solo lo emite al cliente que ha gatillado el evento 
+// // Aqui se definen todos los eventos que cualquier cliente escucha, broadcast emite el evento a TODOS los clientes, mientras
+// // que emit, solo lo emite al cliente que ha gatillado el evento
 
-   socket.on( 'taskAdded', ( task ) =>  {
-       socket.broadcast.emit('taskAdded', task)  
-       socket.emit( 'taskAdded', task )     
-   })
+//    socket.on( 'taskAdded', ( task ) =>  {
+//        socket.broadcast.emit('taskAdded', task)
+//        socket.emit( 'taskAdded', task )
+//    })
 
 
 
-})
+// })
 
 
 mongoose.connect(mongo.uri, { useMongoClient: true })
